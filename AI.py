08d3c4045ddf1ef8ceeb67771c16fa2dd801909d
Degger <1Degger1@vk.com>
2025-03-05 22:56:45 +0400

    add

--- AI.py
+++ AI.py
@@ -2,29 +2,20 @@ from http.client import responses
 
 import openai
 from openai import OpenAI, base_url
+from pyexpat.errors import messages
+
 from config import AI_TOKEN
 
-client = OpenAI(api_key=AI_TOKEN, base_url='https://api.deepseek.com')
+client = OpenAI(api_key='sk-c2a507fa650a4127b06bab29291eb90e', base_url='https://api.deepseek.com')
 
-def chat_with_bot(prompt):
-    try:
-        response = openai.Completion.creat(
-            engine='text-davinci-003',
-            prompt=prompt,
-            max_tokens=200,
-            n=1,
-            stop=None,
-            temperature=0.5
-        )
-        return response.choices[0].text.strip()
-    except Exception as e:
-        return print(f'Произошла ошибка {e}')
+response = client.chat.completions.create(
+    model='deepseek-chat',
+    messages=[
+        {'role': 'system', 'content': 'you are a helpful assistant'},
+        {'role': 'user', 'content': 'hello'}
+    ],
+    max_tokens=1024,
+    temperature=0.7,
+    stream=True)
 
-print('Боте  начало')
-while True:
-    user_input = input('ты : ')
-    if user_input.lower() in ['стоп', 'пока', 'выход']:
-        print('Бот пока! я ушел')
-        break
-    response = chat_with_bot(user_input)
-    print(f'Бот:', {response})
+print(response.choices[0].message.content)
